03d0018006c820ece68fec23b7533a68f3f5f5fc
Jonathan Somer <somer@mail.tau.ac.il>
2016-11-18 21:22:16 +0200

    Create part

---
+++ Server/web/Create/js/main.js
@@ -0,0 +1,6 @@
+// var imageUrl = "images/city hall.jpg";
+// var width = $("#canvas").width();
+// var height = $("#canvas").height();
+//
+// WILL.init(width, height);
+//WILL.initImageLayer(imageUrl, imageWidth, imageHeight);
